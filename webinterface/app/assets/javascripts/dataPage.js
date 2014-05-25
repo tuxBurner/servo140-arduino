c88fe52d21458a859313b6a849118f179157ace2
Sebastian Hardt <tuxburner@boggo.de>
2014-05-25 18:18:40 +0200

    better webcam control

--- webinterface/app/assets/javascripts/dataPage.js
+++ webinterface/app/assets/javascripts/dataPage.js
@@ -52,15 +52,8 @@ var openAddModal = function (route) {
  * Starts the webcam capture
  */
 var startWebcamCapture = function () {
-
     $('#my_result').html('<img src="data:image/jpeg;base64,' + $('#imageData').val() + '"/>');
-
-    Webcam.set({
-        image_format: 'jpeg',
-        jpeg_quality: 90,
-        force_flash: false
-    });
-    Webcam.attach('#my_camera');
+    initializeFileDrop();
 }
 
 /**
@@ -68,7 +61,69 @@ var startWebcamCapture = function () {
  */
 var takeSnapshot = function () {
     var data_uri = Webcam.snap();
-    $('#my_result').html('<img src="' + data_uri + '"/>');
-    var raw_image_data = data_uri.replace(/^data\:image\/\w+\;base64\,/, '');
+    updateImageData(data_uri);
+}
+
+/**
+ * Updates the preview image and sets the data to the input field
+ * @param imgData
+ */
+var updateImageData = function (imgData) {
+    $('#my_result').html('<img src="' + imgData + '"/>');
+    var raw_image_data = imgData.replace(/^data\:image\/\w+\;base64\,/, '');
     $('#imageData').val(raw_image_data);
+}
+
+/**
+ * Initializes the drop zone for the image
+ */
+var initializeFileDrop = function () {
+    // Setup the dnd listeners.
+    var dropZone = document.getElementById('dropZone');
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('drop', handleFileSelect, false);
+}
+
+/**
+ * Handles the event when the user drags the file over the zone
+ * @param evt
+ */
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+/**
+ * Is called when the file is dropped into the zone
+ * @param evt
+ */
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var file = evt.dataTransfer.files[0];
+    if (file.type.match('image.*')) {
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) {
+                updateImageData(e.target.result);
+            };
+        })(file);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(file);
+    }
+}
+
+var startWebcam = function () {
+    $("#webcamControl").show(function () {
+        Webcam.set({
+            image_format: 'jpeg',
+            jpeg_quality: 90,
+            force_flash: false
+        });
+        Webcam.attach('#my_camera');
+    });
 }
